309eb59f98fc0020b4c4bbe2a6c58365bb09fde8
bimanathanael95@gmai.com <bimanathanael95@gmail.com>
2020-08-14 17:35:37 +0700

    server done with redis graphQL

--- server/orcestrator/graphql/schemas/moviesSchema.js
+++ server/orcestrator/graphql/schemas/moviesSchema.js
@@ -1,5 +1,7 @@
 const { ApolloServer, gql } = require('apollo-server');
 const axios = require('axios')
+const Redis = require('ioredis')
+const redis = new Redis()
 
 // A schema is a collection of type definitions (hence "typeDefs")
 // that together define the "shape" of queries that are executed against
@@ -41,31 +43,60 @@ const apiMovieUrl = 'http://localhost:3001/movies'
 const resolvers = {
   Query: {
     movies: async () => {
-      const allMovies = await axios.get(apiMovieUrl)
-      console.log(allMovies.data, 'allMovies.data')
-      return allMovies.data
+      const allMoviesRedis = await redis.get("movies")
+      try {
+        if(allMoviesRedis){
+          return JSON.parse(allMoviesRedis)
+        } 
+        const allMovies = await axios.get(apiMovieUrl)
+        await redis.set("movies", JSON.stringify(allMovies.data))
+        console.log(allMovies.data, 'allMovies.data')
+        return allMovies.data
+      } catch (error) {
+        return error
+      }
     },
     movie: async (_,args) => {
-      console.log(args._id, 'args')
-      const selectedMovie = await axios.get(`${apiMovieUrl}/${args._id}`)
-      return selectedMovie.data
+      const allMoviesRedis = await redis.get("movies")
+      try {
+        if(allMoviesRedis){
+          console.log(JSON.parse(allMoviesRedis))
+          const selectedMovie = JSON.parse(allMoviesRedis).filter(movie => movie._id == args._id) 
+          return selectedMovie[0]
+        } 
+        console.log(args._id, 'args')
+        const selectedMovie = await axios.get(`${apiMovieUrl}/${args._id}`)
+        return selectedMovie.data
+      } catch (error) {
+        return error
+      }
     },
   },
   Mutation: {
     addMovie: async (_,args) => {
       console.log(args.movie,'body')
       const movie = await axios.post(apiMovieUrl,args.movie)
+      const allMoviesRedis = await redis.get("movies")
+      const newData = JSON.parse(allMoviesRedis).concat(movie.data)
+      await redis.set("movies", JSON.stringify(newData))
       return movie.data
     },
     updateMovie: async (_,args) => {
       console.log(args._id,'<<<<id')
       console.log(args.movie,'<<<<body')
       const movie = await axios.put(`${apiMovieUrl}/${args._id}`,args.movie)
+      const allMoviesRedis = await redis.get('movies')
+      const newData = JSON.parse(allMoviesRedis).filter(movie => movie._id != args._id )
+      const updateMovie = newData.concat(movie.data)
+      await redis.set('movies', JSON.stringify(updateMovie))
       return movie.data
     },
     deleteMovie: async (_, args) => {
       console.log(args._id, 'id')
       const movie = await axios.delete(`${apiMovieUrl}/${args._id}`)
+      const allMoviesRedis = await redis.get('movies')
+      const newData = JSON.parse(allMoviesRedis).filter(movie => movie._id != args._id )
+      await redis.set('movies', JSON.stringify(newData))
       return movie.data
     }
   }
--- server/orcestrator/graphql/schemas/tvSeriesSchema.js
+++ server/orcestrator/graphql/schemas/tvSeriesSchema.js
@@ -1,5 +1,7 @@
 const { ApolloServer, gql } = require('apollo-server');
 const axios = require('axios')
+const Redis = require('ioredis')
+const redis = new Redis()
 
 // A schema is a collection of type definitions (hence "typeDefs")
 // that together define the "shape" of queries that are executed against
@@ -41,32 +43,61 @@ const apiTVSeriesUrl = 'http://localhost:3002/tvSeries'
 const resolvers = {
   Query: {
     allTVSeries: async () => {
-      const allTVSeries = await axios.get(apiTVSeriesUrl)
-      console.log(allTVSeries.data, 'allTVSeries.data')
-      return allTVSeries.data
+      const allSeriesRedis = await redis.get("tvSeries")
+      try {
+        if(allSeriesRedis){
+          return JSON.parse(allSeriesRedis)
+        } 
+        const allTVSeries = await axios.get(apiTVSeriesUrl)
+        await redis.set("tvSeries", JSON.stringify(allTVSeries.data))
+        console.log(allTVSeries.data, 'allTVSeries.data')
+        return allTVSeries.data
+      } catch (error) {
+        return error
+      }
     },
     tvSeries: async (_,args) => {
-      console.log(args._id, 'args')
-      const selectedSeries = await axios.get(`${apiTVSeriesUrl}/${args._id}`)
-      return selectedSeries.data
+      const allSeriesRedis = await redis.get("tvSeries")
+      try {
+        if(allSeriesRedis){
+          console.log(JSON.parse(allSeriesRedis))
+          const selectedSeries = JSON.parse(allSeriesRedis).filter(series => series._id == args._id) 
+          return selectedSeries[0]
+        } 
+        console.log(args._id, 'args')
+        const selectedSeries = await axios.get(`${apiTVSeriesUrl}/${args._id}`)
+        return selectedSeries.data
+      } catch (error) {
+        return error
+      }
     },
   },
   Mutation: {
     addSeries: async (_,args) => {
       console.log(args.series,'body')
       const series = await axios.post(apiTVSeriesUrl,args.series)
+      const allSeriesRedis = await redis.get("tvSeries")
+      const newData = JSON.parse(allSeriesRedis).concat(series.data)
+      await redis.set("tvSeries", JSON.stringify(newData))
       return series.data
     },
     updateSeries: async (_,args) => {
       console.log(args._id,'<<<<id')
       console.log(args.series,'<<<<body')
       const series = await axios.put(`${apiTVSeriesUrl}/${args._id}`,args.series)
+      const allSeriesRedis = await redis.get('tvSeries')
+      const newData = JSON.parse(allSeriesRedis).filter(series => series._id != args._id )
+      const updatedSeries = newData.concat(series.data)
+      await redis.set('tvSeries', JSON.stringify(updatedSeries))
       console.log(series.data)
       return series.data
     },
     deleteSeries: async (_, args) => {
       console.log(args._id, 'id')
       const series = await axios.delete(`${apiTVSeriesUrl}/${args._id}`)
+      const allSeriesRedis = await redis.get('tvSeries')
+      const newData = JSON.parse(allSeriesRedis).filter(series => series._id != args._id )
+      await redis.set('tvSeries', JSON.stringify(newData))
       return series.data
     }
   }
